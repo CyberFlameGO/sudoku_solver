79222807fd3b144e412ee3f3be7f01e8bc9878da
Khionu Sybiern <dev@khionu.net>
2019-06-28 03:08:12 -0400

    Fix alignment + lower memory usage

--- src/main.rs
+++ src/main.rs
@@ -7,45 +7,45 @@ use crossbeam_channel::{unbounded, Sender};
 use itertools::Itertools;
 use std::time::Instant;
 
-const ROWS: [[usize; 9]; 9] = [[00, 01, 02, 03, 04, 05, 06, 07, 08],
-                               [09, 10, 11, 12, 13, 14, 15, 16, 17],
-                               [18, 19, 20, 21, 22, 23, 24, 25, 26],
-                               [27, 28, 29, 30, 31, 32, 33, 34, 35],
-                               [36, 37, 38, 39, 40, 41, 42, 43, 44],
-                               [45, 46, 47, 48, 49, 50, 51, 52, 53],
-                               [54, 55, 56, 57, 58, 59, 60, 61, 62],
-                               [63, 64, 65, 66, 67, 68, 69, 70, 71],
-                               [72, 73, 74, 75, 76, 77, 78, 79, 80]];
+const ROWS: [[u8; 9]; 9] = [[00, 01, 02, 03, 04, 05, 06, 07, 08],
+                            [09, 10, 11, 12, 13, 14, 15, 16, 17],
+                            [18, 19, 20, 21, 22, 23, 24, 25, 26],
+                            [27, 28, 29, 30, 31, 32, 33, 34, 35],
+                            [36, 37, 38, 39, 40, 41, 42, 43, 44],
+                            [45, 46, 47, 48, 49, 50, 51, 52, 53],
+                            [54, 55, 56, 57, 58, 59, 60, 61, 62],
+                            [63, 64, 65, 66, 67, 68, 69, 70, 71],
+                            [72, 73, 74, 75, 76, 77, 78, 79, 80]];
 
-const COLS: [[usize; 9]; 9] = [[00, 09, 18, 27, 36, 45, 54, 63, 72],
-                               [01, 10, 19, 28, 37, 46, 55, 64, 73],
-                               [02, 11, 20, 29, 38, 47, 56, 65, 74],
-                               [03, 12, 21, 30, 39, 48, 57, 66, 75],
-                               [04, 13, 22, 31, 40, 49, 58, 67, 76],
-                               [05, 14, 23, 32, 41, 50, 59, 68, 77],
-                               [06, 15, 24, 33, 42, 51, 60, 69, 78],
-                               [07, 16, 25, 34, 43, 52, 61, 70, 79],
-                               [08, 17, 26, 35, 44, 53, 62, 71, 80]];
+const COLS: [[u8; 9]; 9] = [[00, 09, 18, 27, 36, 45, 54, 63, 72],
+                            [01, 10, 19, 28, 37, 46, 55, 64, 73],
+                            [02, 11, 20, 29, 38, 47, 56, 65, 74],
+                            [03, 12, 21, 30, 39, 48, 57, 66, 75],
+                            [04, 13, 22, 31, 40, 49, 58, 67, 76],
+                            [05, 14, 23, 32, 41, 50, 59, 68, 77],
+                            [06, 15, 24, 33, 42, 51, 60, 69, 78],
+                            [07, 16, 25, 34, 43, 52, 61, 70, 79],
+                            [08, 17, 26, 35, 44, 53, 62, 71, 80]];
 
-const SQRS: [[usize; 9]; 9] = [[00, 01, 02, 09, 10, 11, 18, 19, 20],
-                               [03, 04, 05, 12, 13, 14, 21, 22, 23],
-                               [06, 07, 08, 15, 16, 17, 24, 25, 26],
-                               [27, 28, 29, 36, 37, 38, 45, 46, 47],
-                               [30, 31, 32, 39, 40, 41, 48, 49, 50],
-                               [33, 34, 35, 42, 43, 44, 51, 52, 53],
-                               [54, 55, 56, 63, 64, 65, 72, 73, 74],
-                               [57, 58, 59, 66, 67, 68, 75, 76, 77],
-                               [60, 61, 62, 69, 70, 71, 78, 79, 80]];
+const SQRS: [[u8; 9]; 9] = [[00, 01, 02, 09, 10, 11, 18, 19, 20],
+                            [03, 04, 05, 12, 13, 14, 21, 22, 23],
+                            [06, 07, 08, 15, 16, 17, 24, 25, 26],
+                            [27, 28, 29, 36, 37, 38, 45, 46, 47],
+                            [30, 31, 32, 39, 40, 41, 48, 49, 50],
+                            [33, 34, 35, 42, 43, 44, 51, 52, 53],
+                            [54, 55, 56, 63, 64, 65, 72, 73, 74],
+                            [57, 58, 59, 66, 67, 68, 75, 76, 77],
+                            [60, 61, 62, 69, 70, 71, 78, 79, 80]];
 
-const fn get_row(index: usize) -> usize {
+const fn get_row(index: u8) -> u8 {
     index / 9
 }
 
-const fn get_col(index: usize) -> usize {
+const fn get_col(index: u8) -> u8 {
     index % 9
 }
 
-const fn get_sqr(index: usize) -> usize {
+const fn get_sqr(index: u8) -> u8 {
     (get_col(index) / 3) + (get_row(index) / 3) * 3
 }
 
@@ -53,12 +53,12 @@ const fn get_sqr(index: usize) -> usize {
 //    ((get_col(index) % 3) + (get_row(index) * 3)) % 9
 //}
 
-fn get_influences(index: usize) -> Vec<usize> {
+fn get_influences(index: u8) -> Vec<u8> {
     let mut influences = Vec::new();
 
-    ROWS[get_row(index)].iter().for_each(|x| influences.push(x.clone()));
-    COLS[get_col(index)].iter().for_each(|x| influences.push(x.clone()));
-    SQRS[get_sqr(index)].iter().for_each(|x| influences.push(x.clone()));
+    ROWS[get_row(index) as usize].iter().for_each(|x| influences.push(x.clone()));
+    COLS[get_col(index) as usize].iter().for_each(|x| influences.push(x.clone()));
+    SQRS[get_sqr(index) as usize].iter().for_each(|x| influences.push(x.clone()));
 
     influences.iter().filter(|x| **x != index).unique().map(|x| x.clone()).collect()
 }
@@ -148,8 +148,8 @@ pub struct Board {
 
 #[derive(Clone, Copy)]
 pub struct Tile {
-    pub index: usize,
-    pub value: Option<usize>,
+    pub index: u8,
+    pub value: Option<u8>,
     pub hints: [bool; 9],
 }
 
@@ -161,7 +161,7 @@ impl Board {
         let collected = src.lines()
             .filter(|s| !s.is_empty())
             .flat_map(|content| {
-                let mut index = 0usize;
+                let mut index = 0u8;
                 content.chars()
                     .filter(|c| {
                         match c {
@@ -174,7 +174,7 @@ impl Board {
                         let hints = match &value {
                             Some(v) => {
                                 let mut h = [false; 9];
-                                h[*v-1] = true;
+                                h[*v as usize] = true;
                                 h
                             },
                             None => [true; 9],
@@ -194,7 +194,7 @@ impl Board {
             return Err(String::from("More than 81 tiles collected"))
         }
 
-        collected.for_each(|t| tiles[t.index] = t);
+        collected.for_each(|t| tiles[t.index as usize] = t);
 
         Ok(Board { tiles })
     }
@@ -215,10 +215,10 @@ impl Board {
                     continue;
                 }
 
-                for inf in get_influences(tile_index) {
-                    match self.tiles[inf].value {
+                for inf in get_influences(tile_index as u8) {
+                    match self.tiles[inf as usize].value {
                         Some(v) => {
-                            let hint = &mut self.tiles[tile_index].hints[v];
+                            let hint = &mut self.tiles[tile_index].hints[v as usize];
                             if *hint {
                                 *hint = false;
 
@@ -236,7 +236,7 @@ impl Board {
                 match hints {
                     1 => {
                         let val = self.tiles[tile_index].hints.iter()
-                            .position(|h| *h);
+                            .position(|h| *h).map(|v| v as u8);
 
                         self.tiles[tile_index].value = val.clone();
 
@@ -272,9 +272,9 @@ impl Board {
 
                     let mut branch = self.clone();
 
-                    let branchtile = &mut branch.tiles[branchtile.index];
+                    let branchtile = &mut branch.tiles[branchtile.index as usize];
 
-                    branchtile.value = Some(i);
+                    branchtile.value = Some(i as u8);
                     branchtile.hints = Default::default();
                     branchtile.hints[i] = true;
 
@@ -293,17 +293,17 @@ pub enum BoardResult {
     Failed(Board)
 }
 
-fn char_to_maxnine(c: char) -> Option<usize> {
+fn char_to_maxnine(c: char) -> Option<u8> {
     match c {
-        '1' => Some(1),
-        '2' => Some(2),
-        '3' => Some(3),
-        '4' => Some(4),
-        '5' => Some(5),
-        '6' => Some(6),
-        '7' => Some(7),
-        '8' => Some(8),
-        '9' => Some(9),
+        '1' => Some(0),
+        '2' => Some(1),
+        '3' => Some(2),
+        '4' => Some(3),
+        '5' => Some(4),
+        '6' => Some(5),
+        '7' => Some(6),
+        '8' => Some(7),
+        '9' => Some(8),
         _   => None,
     }
 }
